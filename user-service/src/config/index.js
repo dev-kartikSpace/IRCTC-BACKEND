e91f94a54bfd562b6ff8ea271cc85b66b842f36f
kartik <kartik200327@gmail.com>
2026-07-29 17:34:36 +0530

    fix config index file to import or needed variables

--- user-service/src/config/index.js
+++ user-service/src/config/index.js
@@ -1,17 +1,29 @@
 require('dotenv').config();
 const config = {
-  SERVICE_NAME: require("../../package.json").name,
+  SERVICE_NAME: require('../../package.json').name,
   PORT: Number(process.env.PORT) || 4001,
   NODE_ENV: process.env.NODE_ENV || "development",
   LOG_LEVEL: process.env.LOG_LEVEL || "info",
+  DATABASE_URL: process.env.DATABASE_URL,
   REDIS_URL: process.env.REDIS_URL,
   ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
 
   OTP_TTL: process.env.OTP_TTL || 300,
   OTP_RATE_MAX_PER_HOUR: process.env.OTP_RATE_MAX_PER_HOUR || 5,
   OTP_MAX_VERIFY_ATTEMPTS: process.env.OTP_MAX_VERIFY_ATTEMPTS || 5,
-
   OTP_HMAC_SECRET: process.env.OTP_HMAC_SECRET || "09dc0abbb2961391d822610b31b912e3231d4d2745c76b1ef4765af4c62f6079",
+
+  MAIL_SEND: process.env.MAIL_SEND,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+
 };
+
+if (!config.SENDGRID_API_KEY) {
+  throw new Error('SENDGRID_API_KEY missing');
+}
+
+if (!config.MAIL_SEND) {
+  throw new Error('MAIL_SEND missing');
+}
 
 module.exports = { config };
